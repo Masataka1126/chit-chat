6529319750381ad3323281369e513d570b32219a
Masataka1126 <54941936+Masataka1126@users.noreply.github.com>
2021-09-25 15:00:39 +0900

    initial　commit

---
+++ src/main/java/org/noname/onestep/insignificanttalk/InsignificanttalkApplication.java
@@ -0,0 +1,13 @@
+package org.noname.onestep.insignificanttalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsignificanttalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsignificanttalkApplication.class, args);
+	}
+
+}
